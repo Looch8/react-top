36cda57f25472cb29b9d57b4a7bb8f589d638101
Luke Wheldale <lukewheldale@gmail.com>
2023-09-14 21:40:17 +0930

    playing around with exporting and importing components

---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+function FavFood() {
+	return <h1>My favorite food is Pizza</h1>;
+}
+
+export function FavDrink() {
+	return <h1>My favorite drink is Coke</h1>;
+}
+
+export let FavDessert = "My favourite dessert is ice cream";
+export function FavFruit() {
+	return ["apple", "banana", "orange"];
+}
+export default FavFood;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Greeting from "./Greeting.jsx";
+import Food, { FavDrink, FavDessert, FavFruit } from "./Food.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Food />
+		<FavDrink />
+		<div>
+			{" "}
+			<h1>{FavDessert}</h1>
+		</div>
+		<div>
+			<li>
+				<FavFruit />
+			</li>
+		</div>
+	</React.StrictMode>
+);
